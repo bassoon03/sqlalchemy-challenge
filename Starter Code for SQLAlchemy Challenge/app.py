d814531decafc81be78f047ec6473b679dd37121
Brent Lang <bassoon03@gmail.com>
2024-03-19 14:04:39 -0700

    Dynamic routes finished

--- Starter Code for SQLAlchemy Challenge/app.py
+++ Starter Code for SQLAlchemy Challenge/app.py
@@ -77,14 +77,19 @@ def welcome():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
 
+    # Precipitation query
     precipitation = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= one_year_ago)
-    
+
+    # Turn precipitation query results into data frame
     precipitation_df = pd.DataFrame(precipitation, columns = ("Date", "Precipitation"))
     
+    # Turn data frame into dictionary
     precipitation_dict = precipitation_df.set_index('Date')['Precipitation'].to_dict()
 
+    # Jsonify dictionary
     return jsonify(precipitation_dict)
 
+    # Close session
     session.close()
 
 
@@ -92,16 +97,20 @@ def precipitation():
 @app.route("/api/v1.0/stations")
 def stations():
 
-    
+    # Query the stations
     station_query = session.query(Station.station)
 
+    # Empty list for stations
     station_list = []
     
+    # Iterate through station_query and append each station to station_list
     for result in station_query:
         station_list.append(result.station)
 
+    # Jsonify station_list
     return jsonify(station_list)
 
+    # Close session
     session.close()
 
 
@@ -109,15 +118,21 @@ def stations():
 @app.route("/api/v1.0/tobs")
 def tobs():
 
+    # Query temperature data, filter results to get everything from one_year_ago to the most recent date,
+    # then filter to get results for the most active station
     temperature_data = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= one_year_ago).\
-    filter(Measurement.station == h_station[0])
+        filter(Measurement.station == h_station[0])
 
+    # Turn query results into a data frame
     temperature_df = pd.DataFrame(temperature_data, columns = ("Date", "Tobs"))
 
+    # Get Tobs data from data frame and turn it into a list
     temperatures = temperature_df["Tobs"].tolist()
 
+    # Jsonify the temperatures list
     return jsonify(temperatures)
 
+    # Close session
     session.close()
 
 
@@ -125,29 +140,38 @@ def tobs():
 @app.route("/api/v1.0/<start>")
 @app.route("/api/v1.0/<start>/<end>")
 def start_stop(start, end):
-    
+    # Date values for start and end must be in the form 'YYYY-MM-DD'.
+    # For first route (remember to type 'None' where <end> would be):
     if end == None:
-
-        desired_data = session.query(Measurement.tobs, Measurement.date).filter(Measurement.date >= start)
-            
-        desired_data_df = pd.DataFrame(desired_data, columns = ('Tobs', 'Date'))
         
-    else:
+        # Query the temperature observations and filter by start date
+        desired_data = session.query(Measurement.tobs, Measurement.date).filter(Measurement.date >= start)
 
+        # Turn query results into data frame  
+        desired_data_df = pd.DataFrame(desired_data, columns = ('Tobs', 'Date'))
+
+
+    # For second route:    
+    else:
+        # Query the temperature observations, filter by start date through end date.
         desired_data = session.query(Measurement.tobs, Measurement.date).filter(Measurement.date >= start).\
             filter(Measurement.date <= end)
-            
+        
+        # Turn query results into data frame
         desired_data_df = pd.DataFrame(desired_data, columns = ('Tobs', 'Date'))
             
-    
+    # Average temperature (tavg), minimum temperature (tmin), maximum temperature (tmax)
     tavg = desired_data_df['Tobs'].mean()
     tmin = min(desired_data_df['Tobs'])
     tmax = max(desired_data_df['Tobs'])
 
+    # Close session
     session.close()
 
+    # Puts tavg, tmin, and tmax into list of f strings
     results_list = [f"Average Temperature: {tavg}", f"Minimum Temperature: {tmin}", f"Maximum Temperature: {tmax}"]
 
+    # results_list is returned
     return results_list
 
     
